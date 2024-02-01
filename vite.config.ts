80c1a0af636c4c5ece15a54cb63e070f7dd219e2
Samir El Hassan <samirelhassann@gmail.com>
2024-02-01 01:24:17 -0300

    Adjusting cross env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const base = process.env.VITE_BASE || '/';
+
 export default defineConfig({
   plugins: [react()],
-  base: "/ignite-blog"
+  base: base
 })
